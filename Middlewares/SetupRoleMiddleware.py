54e2a1ae7a4c51854c03aacd5a92aee1bfb7331f
TAPAKAHOKOT <tapakahokot@gmail.com>
2022-04-16 19:30:26 +0300

    (fix) bug

--- Middlewares/SetupRoleMiddleware.py
+++ Middlewares/SetupRoleMiddleware.py
@@ -15,17 +15,23 @@ class SetupRoleMiddleware(BaseMiddleware):
     async def on_pre_process_message(self, message: Message, data: dict):
         admins = map(int, getenv("ADMINS").split(','))
         chat_id = message.from_user['id']
+        role: Role = None
 
         if chat_id not in admins:
             return
 
         with Session(engine) as session, session.begin():
-            user: User = User.find_by_chat_id(session, chat_id)
-            role: Role = Role.find_by_role(session, 'root')
+            user: User = data['user']
+            role = Role.find_by_role(session, 'root')
 
             if user and role:
                 user.role_id = role.id
                 session.add(user)
+
+                data['user_relations']['role'] = user.role
+            
+            session.expunge_all()
+
 
 
         
